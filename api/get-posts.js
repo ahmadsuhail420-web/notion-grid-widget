7ad4235a5978ad7cf838b978ef896d043fdf54e6
ahmadsuhail420-web <ahmadsuhail420@gmail.com>
2026-02-08 09:16:57 +0300

    Create get-posts.js

---
+++ api/get-posts.js
@@ -0,0 +1,17 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export default async function handler(req, res) {
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+    });
+
+    res.status(200).json(response.results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
