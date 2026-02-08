a1efae0f5fa96760c1ac0257e0ff44606416b97f
ahmadsuhail420-web <ahmadsuhail420@gmail.com>
2026-02-08 11:15:07 +0300

    Update get-posts.js

--- api/get-posts.js
+++ api/get-posts.js
@@ -1,17 +1,38 @@
-import { Client } from "@notionhq/client";
-
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
-
 export default async function handler(req, res) {
-  try {
-    const response = await notion.databases.query({
-      database_id: process.env.NOTION_DATABASE_ID,
-    });
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
 
-    res.status(200).json(response.results);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const posts = response.results.map(page => {
+
+    const attachment =
+      page.properties["Attachment"]?.files?.[0]?.file?.url || null;
+
+    const video =
+      page.properties["Media/Video"]?.url || null;
+
+    const type =
+      page.properties["Type"]?.multi_select?.map(t => t.name) || [];
+
+    const pinned =
+      page.properties["Pin"]?.checkbox || false;
+
+    const publishDate =
+      page.properties["Publish Date"]?.date?.start || null;
+
+    const name =
+      page.properties["Name"]?.rich_text?.[0]?.plain_text || "";
+
+    return {
+      id: page.id,
+      name,
+      publishDate,
+      attachment,
+      video,
+      type,
+      pinned
+    };
+  });
+
+  res.status(200).json(posts);
 }
