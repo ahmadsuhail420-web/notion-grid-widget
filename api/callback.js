0d89a46a78d43bd8a34ac5d5efa957f8704867c0
ahmadsuhail420-web <ahmadsuhail420@gmail.com>
2026-02-14 16:58:34 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const { code, slug } = req.query;
-
+const slug = state;
   if (!code || !slug) {
     return res.status(400).send("Missing code or slug");
   }
